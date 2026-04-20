4b44ef6e8c9b6362d6fea6bdd075fa4be89b39ed
yasinarafat342 <yasingazi343@gmail.com>
2026-04-21 02:04:51 +0600

    mainjsx editing

--- keen-keeper/src/main.jsx
+++ keen-keeper/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // এটি যোগ করুন
+import { BrowserRouter } from 'react-router-dom' 
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* <App /> কে BrowserRouter দিয়ে ঘিরে দিন */}
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </React.StrictMode>,
