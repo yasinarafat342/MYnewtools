fa1b2e7bf9e9f5f557bc1bfbb30616f61060e280
yasinarafat342 <yasingazi343@gmail.com>
2026-04-21 02:03:52 +0600

    adding on feacturs

--- keen-keeper/src/App.jsx
+++ keen-keeper/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Timeline from "./pages/Timeline";
 import Stats from "./pages/Stats";
 import FriendDetail from "./pages/FriendDetail";
-import NotFound from "./pages/NotFound"; // NotFound পেজটি ইম্পোর্ট করলাম
+import NotFound from "./pages/NotFound";
 import { InteractionProvider } from "./InteractionContext";
 
 function App() {
@@ -15,16 +15,16 @@ function App() {
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            {/* Home Page: এখানে লোডিং অ্যানিমেশন থাকবে */}
+           
             <Route path="/" element={<Home />} />
             
             <Route path="/timeline" element={<Timeline />} />
             <Route path="/stats" element={<Stats />} />
             
-            {/* Dynamic Route for Friend Details */}
+           
             <Route path="/friend/:id" element={<FriendDetail />} />
 
-            {/* 404 Page: যেকোনো ভুল পাথের জন্য এই পেজটি দেখাবে */}
+            
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
